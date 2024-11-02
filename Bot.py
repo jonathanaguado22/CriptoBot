28e500fec30cfd58c3394a9d551a69b376d4094c
Cesar Aguado <cesaraguado123@gmail.com>
2024-11-01 20:48:49 -0400

    talib install

--- Bot.py
+++ Bot.py
@@ -1,2 +1,10 @@
-import talib
-print(talib.__version__)
+# %% Librerias
+
+import talib as ta
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+
+plt.style.use('bmh')
+
+aapl = yf.download('AAPL', '2024-01-01', '2024-10-31')
