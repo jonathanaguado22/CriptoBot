c0b19929b56a15c2d29d2dbabac833150913e37e
Cesar Aguado <cesaraguado123@gmail.com>
2024-11-02 17:58:39 -0400

    moving stockings

--- Bot.py
+++ Bot.py
@@ -1,6 +1,6 @@
 # %% Librerias
-
-import talib
+import pandas
+import pandas_ta as ta
 import matplotlib.pyplot as plt
 import yfinance as yf
 
@@ -12,10 +12,13 @@ aapl = yf.download('AAPL', '2023-01-01', '2024-10-31')
 
 # %% Medias Moviles
 
-aapl['SMA'] = tab.SMA(aapl['Close'].values, timeperiod=21)
-aapl['EMA'] = tab.EMA(aapl['Close'].values, timeperiod=55)
+aapl['SMA'] = ta.sma(aapl['Close'], length=20)
+
+aapl['EMA'] = ta.ema(aapl['Close'], length=20)
+
 
 
 # Plot
 
 aapl[['Close', 'SMA', 'EMA']].plot(figsize=(10, 6))
+plt.show()
