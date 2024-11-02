8e38b030aca5ebe15c253dd658b0950870d85b90
Cesar Aguado <cesaraguado123@gmail.com>
2024-11-01 21:59:04 -0400

    talib problem

--- Bot.py
+++ Bot.py
@@ -1,10 +1,21 @@
 # %% Librerias
 
-import talib as ta
+import talib
 import matplotlib.pyplot as plt
 import yfinance as yf
 
 
 plt.style.use('bmh')
 
-aapl = yf.download('AAPL', '2024-01-01', '2024-10-31')
+aapl = yf.download('AAPL', '2023-01-01', '2024-10-31')
+
+
+# %% Medias Moviles
+
+aapl['SMA'] = tab.SMA(aapl['Close'].values, timeperiod=21)
+aapl['EMA'] = tab.EMA(aapl['Close'].values, timeperiod=55)
+
+
+# Plot
+
+aapl[['Close', 'SMA', 'EMA']].plot(figsize=(10, 6))
