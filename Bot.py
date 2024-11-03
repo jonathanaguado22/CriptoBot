cb19d743792c44f4f165711a0f08c9d3cf977cf8
Cesar Aguado <cesaraguado123@gmail.com>
2024-11-02 21:52:36 -0400

    moving stockings

--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
 # %% Librerias
-import pandas
+import pandas as pd
 import pandas_ta as ta
 import matplotlib.pyplot as plt
 import yfinance as yf
@@ -7,18 +7,19 @@ import yfinance as yf
 
 plt.style.use('bmh')
 
-aapl = yf.download('AAPL', '2023-01-01', '2024-10-31')
-
+# Descargar datos
+aapl = yf.download('AAPL', '2019-01-01', '2021-05-03')
+aapl.dropna(inplace=True)
+# Verifica los primeros datos descargados
+print(aapl.head())
 
 # %% Medias Moviles
+aapl['SMA'] = ta.sma(aapl['Close'], 50 )
+aapl['EMA'] = ta.ema(aapl['Close'], 50)
 
-aapl['SMA'] = ta.sma(aapl['Close'], length=20)
-
-aapl['EMA'] = ta.ema(aapl['Close'], length=20)
-
-
-
+# Verifica los primeros datos con SMA y EMA calculados
+print(aapl[['Close', 'SMA', 'EMA']].head())
+print(aapl.head())
 # Plot
-
 aapl[['Close', 'SMA', 'EMA']].plot(figsize=(10, 6))
 plt.show()
