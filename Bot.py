ba1eae67ed84f6e5791e4259e7128aae2116684b
Cesar Aguado <cesaraguado123@gmail.com>
2024-11-04 08:48:32 -0400

    moving stockings finish

--- Bot.py
+++ Bot.py
@@ -1,25 +1,35 @@
-# %% Librerias
+# %% Librerías
 import pandas as pd
 import pandas_ta as ta
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-
 plt.style.use('bmh')
 
 # Descargar datos
-aapl = yf.download('AAPL', '2019-01-01', '2021-05-03')
-aapl.dropna(inplace=True)
+appl = "AAPL"
+appl_data = yf.download(appl, start="2023-01-01", end="2023-12-31")
+
 # Verifica los primeros datos descargados
-print(aapl.head())
+print("Primeras filas de los datos descargados:")
+print(appl_data.head())
 
-# %% Medias Moviles
-aapl['SMA'] = ta.sma(aapl['Close'], 50 )
-aapl['EMA'] = ta.ema(aapl['Close'], 50)
+if isinstance(appl_data.columns, pd.MultiIndex):
+    appl_data.columns = appl_data.columns.get_level_values(0)
+# %% Medias Móviles
 
-# Verifica los primeros datos con SMA y EMA calculados
-print(aapl[['Close', 'SMA', 'EMA']].head())
-print(aapl.head())
-# Plot
-aapl[['Close', 'SMA', 'EMA']].plot(figsize=(10, 6))
+appl_data['SMA'] = ta.sma(appl_data['Close'], length=50)
+appl_data['EMA'] = ta.ema(appl_data['Close'], length=50)
+
+# Mostrar las primeras y últimas filas de SMA y EMA calculadas
+
+print(appl_data[['Close', 'SMA', 'EMA']].head(10))
+
+
+print(appl_data[['Close', 'SMA', 'EMA']].dropna().tail())
+
+# %% Plot
+# Graficar los datos de cierre, SMA y EMA
+appl_data[['Close', 'SMA', 'EMA']].plot(figsize=(10, 6))
+plt.legend()
 plt.show()
