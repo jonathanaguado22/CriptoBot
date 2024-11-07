26c2ac0524a15ee2817f8a7930d7638e50869735
Cesar Aguado <cesaraguado123@gmail.com>
2024-11-06 21:19:46 -0400

    install Binance

--- Bot_Binance.py
+++ Bot_Binance.py
@@ -1,1 +1,7 @@
 # Bot para el mercado de criptomoneda con Binance
+
+from binance.spot import Spot
+
+
+client = Spot(api_key='0rS0rqspDFp53oaO8uaQBVS18d73FZhhoNm7MO6BsGzdHcnsh1hTPs8rVkDf4Gu5', api_secret='qND6hVEiVdmg02JV7XTsNHA8XCP4RWKD7p3g96jgKig97w2ZgcbF33hXar9Cu7Pj')
+
