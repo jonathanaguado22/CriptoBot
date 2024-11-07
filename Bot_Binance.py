9bda80b65dde3a0858f126b08afb87a7cc09e043
Cesar Aguado <cesaraguado123@gmail.com>
2024-11-07 13:42:03 -0400

    config

--- Bot_Binance.py
+++ Bot_Binance.py
@@ -1,7 +1,9 @@
 # Bot para el mercado de criptomoneda con Binance
 
 from binance.spot import Spot
+import config
 
+client = Spot(api_key= config.API_KEY,
+              api_secret= config.API_SECRET)
 
-client = Spot(api_key='0rS0rqspDFp53oaO8uaQBVS18d73FZhhoNm7MO6BsGzdHcnsh1hTPs8rVkDf4Gu5', api_secret='qND6hVEiVdmg02JV7XTsNHA8XCP4RWKD7p3g96jgKig97w2ZgcbF33hXar9Cu7Pj')
 
